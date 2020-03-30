ca82ec062dccf4c34da42317eecab6696f051bc3
Ondřej Hliba <hliba.ondra@live.com>
2020-03-30 12:01:13 +0200

    Add menu structure

---
+++ Menu.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace tic_tac_toe
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnPCGame = new System.Windows.Forms.Button();
+            this.btnHumanGame = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(275, 44);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Tic Tac Toe";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnPCGame
+            // 
+            this.btnPCGame.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPCGame.Location = new System.Drawing.Point(12, 56);
+            this.btnPCGame.Name = "btnPCGame";
+            this.btnPCGame.Size = new System.Drawing.Size(275, 70);
+            this.btnPCGame.TabIndex = 1;
+            this.btnPCGame.Text = "New game against PC";
+            this.btnPCGame.UseVisualStyleBackColor = true;
+            // 
+            // btnHumanGame
+            // 
+            this.btnHumanGame.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnHumanGame.Location = new System.Drawing.Point(12, 132);
+            this.btnHumanGame.Name = "btnHumanGame";
+            this.btnHumanGame.Size = new System.Drawing.Size(275, 70);
+            this.btnHumanGame.TabIndex = 1;
+            this.btnHumanGame.Text = "New game against human";
+            this.btnHumanGame.UseVisualStyleBackColor = true;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnHelp.Location = new System.Drawing.Point(12, 208);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(275, 70);
+            this.btnHelp.TabIndex = 1;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSettings.Location = new System.Drawing.Point(12, 284);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(275, 70);
+            this.btnSettings.TabIndex = 1;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEnd.Location = new System.Drawing.Point(12, 360);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(275, 70);
+            this.btnEnd.TabIndex = 1;
+            this.btnEnd.Text = "End";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 450);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnHumanGame);
+            this.Controls.Add(this.btnPCGame);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnPCGame;
+        private System.Windows.Forms.Button btnHumanGame;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnEnd;
+    }
+}
--- Menu.cs
+++ Menu.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace tic_tac_toe
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
         }
-
     }
 }
